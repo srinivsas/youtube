5f2eb914bf58f91d7f5798ca3e206c28416acec5
srinivsas <srinivas315674@gmail.com>
2023-03-25 19:18:55 +0530

    commit

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import MainContainer from "./MainContainer";
 import SideBar from "./SideBar";
 
 const BodyContainer = () => {
   return (
     <div className="flex">
       <SideBar />
-      {/* <MainContainer /> */}
       <Outlet />
     </div>
   );
